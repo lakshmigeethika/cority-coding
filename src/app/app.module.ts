176d91115d442b7cf613fdaf7d82ad1614b9a65f
Lakshmi Geethika Tripuramallu <geethika.tripuramallu@gmail.com>
2019-05-01 18:07:54 -0400

    hardcoded list component. TODO - click function for load more, plus button, mobile styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list.ts
@@ -0,0 +1,8 @@
+export class List{
+  imgURL: string;
+  heading: string;
+  desc1: string;
+  desc2: string;
+  todayDate: number;
+
+}
